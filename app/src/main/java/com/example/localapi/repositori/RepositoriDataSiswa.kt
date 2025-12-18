d3ce4cd0f5dfc718e5a1e2246093be530b216e45
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-12-18 16:34:32 +0700

    Mengganti nama fungsi getSiswa menjadi getDataSiswa

--- app/src/main/java/com/example/localapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/localapi/repositori/RepositoriDataSiswa.kt
@@ -4,7 +4,7 @@ import com.example.localapi.apiservice.ServiceApiSiswa
 import com.example.localapi.modeldata.DataSiswa
 
 interface RepositoriDataSiswa{
-    suspend fun getSiswa(): List<DataSiswa>
+    suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
 
 //    suspend fun getSatuSiswa(id: Int): DataSiswa
@@ -15,9 +15,9 @@ interface RepositoriDataSiswa{
 class JaringanRepositoriDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoriDataSiswa{
-    override suspend fun getSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-//    override suspend fun getSatuSiswa(id: Int): DataSiswa
-//    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
-//    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
+//   override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+//   override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+//    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
