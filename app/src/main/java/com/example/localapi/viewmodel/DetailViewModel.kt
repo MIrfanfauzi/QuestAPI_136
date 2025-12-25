aba26ed34d0670236964e0337f36b9bd333c1ff9
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-12-25 23:21:59 +0700

    Membuat DetailViewModel dan StatusUIDetail

--- app/src/main/java/com/example/localapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,25 @@
+@file:OptIn(kotlinx.serialization.InternalSerializationApi::class)
+
 package com.example.localapi.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localapi.modeldata.DataSiswa
+import com.example.localapi.repositori.RepositoriDataSiswa
+import com.example.localapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
 
