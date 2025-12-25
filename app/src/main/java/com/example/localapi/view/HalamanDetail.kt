ebd2f90d2fec16eaa79b179fbdcdb9bd2b772d96
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-12-26 00:15:22 +0700

    Membuat DetailSiswaScreen

--- app/src/main/java/com/example/localapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/localapi/view/HalamanDetail.kt
@@ -1,2 +1,37 @@
 package com.example.localapi.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.localapi.R
+import com.example.localapi.uicontroller.route.DestinasiDetail
+import com.example.localapi.viewmodel.DetailViewModel
+import com.example.localapi.viewmodel.StatusUIDetail
+import com.example.localapi.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(
+        factory = PenyediaViewModel.Factory
+    )
+) {
+}
+
