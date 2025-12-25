a08d715eb38ea15310e73a0f0001981663227cb9
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-12-25 23:16:17 +0700

    Membuat EditViewModel dengan SavedStateHandle dan RepositoriDataSiswa

--- app/src/main/java/com/example/localapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localapi/viewmodel/EditViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.localapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localapi.modeldata.DetailSiswa
+import com.example.localapi.modeldata.UIStateSiswa
+import com.example.localapi.modeldata.toDataSiswa
+import com.example.localapi.modeldata.toUiStateSiswa
+import com.example.localapi.repositori.RepositoriDataSiswa
+import com.example.localapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoriDataSiswa
+) : ViewModel() {
+
+}
